cad78a2ade4b36fd42fd93ec7338dbf6c3a78f4a
KolpashchikovRYu <kromjk@gmail.com>
2022-03-28 13:56:14 +0500

    edit task

--- task_1_1.py
+++ task_1_1.py
@@ -1,7 +1,5 @@
 # 1. Реализовать вывод информации о промежутке времени в зависимости от его продолжительности duration в секундах.
 duration = int(input('Введите размер отрезка времени в секундах: '))
-#print(duration)
-
 #Дни
 duration_days = duration // (3600 * 24)
 #print(f'{duration_days} д.')
@@ -14,10 +12,6 @@ duration_min = ((duration % (3600 * 24)) % 3600) // 60
 #Секунды
 duration_sec = duration % 60
 #print(f'{duration_sec} сек.')
-
-#Общий вывод
-#print(f'В введённом отрезке времени: {duration_days} д., {duration_hour} ч., {duration_min} мин., {duration_sec} сек.')
-
 #решение задания
 if (duration_days or duration_hour or duration_min) == 0:
     print(f'В введённом отрезке времени: {duration_sec} сек.')
--- task_1_2.py
+++ task_1_2.py
@@ -1,8 +1,6 @@
 numbers = [] #Создаём список
-
 for i in range(1,1000,2): #Делаем перебор от 1 до 10 с шагом 2, что бы брать только нечётные числа
     numbers.append(i**3) #Добволяем к каждому из чисел возведение в куб
-#print(numbers) #Выводим результат сформированного списка
 
 sum_cube = []
 for item in numbers: #Перебор элементов списка
@@ -12,10 +10,9 @@ for item in numbers: #Перебор элементов списка
         list_sum_items += int(s[j]) #Сложение эдементов строки с приобразованием в цплочисленное число
     if list_sum_items % 7 == 0: #Проверка на деление без остатка суммы чисел числа.
         sum_cube.append(item) #Если сумма чисел из которых состоит элемент делится на 7 без остатка, то добовляем в новый список sum_cube
-#print(sum_cube)
 list_sum = 0
-for h in sum_cube:
-    list_sum += h
+for i in sum_cube:
+    list_sum += i
 print(f'Сумма чисел из списка возведённых в куб, сумма цифр которых делится на 7 равна: {list_sum}')
 
 
@@ -25,8 +22,6 @@ print(f'Сумма чисел из списка возведённых в куб
 numbers2 = []
 for i in range(1,1000,2): #Делаем перебор от 1 до 10 с шагом 2, что бы брать только нечётные числа
     numbers2.append((i ** 3) + 17) #Добволяем к каждому из чисел возведение в куб
-#print(numbers2) #Выводим результат сформированного списка
-
 sum_cube_17=[]
 for item in numbers2:
     str_list = str(item)
@@ -35,8 +30,6 @@ for item in numbers2:
         list_sum_items += int(str_list[j])
     if list_sum_items % 7 == 0:
         sum_cube_17.append(item)
-#print(sum_cube_17)
-
 list_sum = 0
 for h in sum_cube_17:
     list_sum += h
